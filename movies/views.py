b1a0819588cb512862dc267be4507a0fd2193f9b
adm1ngit <4dm1n217@gmail.com>
2023-08-17 10:49:27 -0700

    finished

---
+++ movies/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
+
+from movies.models import Movie
+
+
+class MoviesView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = "url"
+
